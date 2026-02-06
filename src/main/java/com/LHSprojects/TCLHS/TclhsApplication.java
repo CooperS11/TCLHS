d6508b09a0452d2be50a56347c206c114203a59b
CooperS11 <csvart11@gmail.com>
2026-02-06 09:38:24 -0800

    Start

---
+++ src/main/java/com/LHSprojects/TCLHS/TclhsApplication.java
@@ -0,0 +1,13 @@
+package com.LHSprojects.TCLHS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TclhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TclhsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LHSprojects/TCLHS/TclhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LHSprojects.TCLHS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TclhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
