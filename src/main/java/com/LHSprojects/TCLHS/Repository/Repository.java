503ae1739f77623be755da886f7dd0a614ef6409
Err0r-402 <pckaxe96@gmail.com>
2026-03-03 15:51:33 -0800

    Added Repository.java

---
+++ src/main/java/com/LHSprojects/TCLHS/Repository/Repository.java
@@ -0,0 +1,4 @@
+//Temporary storage of public data using ConcurrentHashMap for easy data retrieval in the backend. 
+//Method to retrive public profile data from database and give it to a sorting algorithm in service.
+//Method to retrive chats if we're doing that?
+//The cache updates when data is commited to actual DB
