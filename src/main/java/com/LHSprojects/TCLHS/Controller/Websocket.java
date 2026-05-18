31c549879dbdb5cc7bbc9b9ab706d62112414b1f
artemisbeams <artemisbeams@gmail.com>
2026-05-18 09:46:00 -0700

    Co-authored-by: Lin, Briana - LIN 12 000 <1brlin1@seattleschools.org>

--- src/main/java/com/LHSprojects/TCLHS/Controller/Websocket.java
+++ src/main/java/com/LHSprojects/TCLHS/Controller/Websocket.java
@@ -1,5 +1,16 @@
 package com.LHSprojects.TCLHS.Controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.resilience.annotation.ConcurrencyLimit;
+
+@controller
 public class Websocket {
-    
+    @MessageMapping("/getTutors");
+    @SendTo("/topic/tutors")
+    public void getTutors() {
+        // This method will be called when a message is sent to the "/getTutors" endpoint.
+        // You can implement logic here to retrieve and send tutor data to the client.
+        
+    }
 }
