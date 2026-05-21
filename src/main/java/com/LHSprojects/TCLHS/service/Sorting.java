bb2bd5efc23fc73a497f0d0ce2222bc45bded564
CooperS11 <csvart11@gmail.com>
2026-05-21 09:58:37 -0700

    some link and sorting changes

--- src/main/java/com/LHSprojects/TCLHS/service/Sorting.java
+++ src/main/java/com/LHSprojects/TCLHS/service/Sorting.java
@@ -1,11 +1,14 @@
 package com.LHSprojects.TCLHS.service;
 import java.util.ArrayList;
-/*
+
+import com.LHSprojects.TCLHS.model.Student;
+import com.LHSprojects.TCLHS.model.Tutor;
+/* 
 public class Sorting {
     //calculates how many courses match between a student and tutor
     public int match(Student student, Tutor tutor) {
-        ArrayList<Integer> s = student.getCourses();
-        ArrayList<Integer> t = tutor.getCourses();
+        ArrayList<String> s = student.getCourses();
+        ArrayList<String> t = tutor.getCourses();
         int match = 0;
 
         for(int i = 0; i < t.size(); i++) {
@@ -21,8 +24,8 @@ public class Sorting {
 
     //returns a list of matching courses
     public ArrayList<Integer> matchIDs(Student student, Tutor tutor) {
-        ArrayList<Integer> s = student.getCourses();
-        ArrayList<Integer> t = tutor.getCourses();
+        ArrayList<String> s = student.getCourses();
+        ArrayList<String> t = tutor.getCourses();
         ArrayList<Integer> match = new ArrayList<Integer>();
 
         for(int i = 0; i < t.size(); i++) {
